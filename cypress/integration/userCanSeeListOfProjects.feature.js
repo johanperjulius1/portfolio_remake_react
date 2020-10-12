cbab3406ded04d7482935946f1aaedf3dd7d6662
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-10-12 08:55:26 +0200

    Adding class component and a state that I am naming projects, adding render and returnwith variables projects and projectsList.

---
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -0,0 +1,40 @@
+describe("User can see a list of projects", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+    cy.get("#projects-tab").click();
+  });
+
+  it("displays first project", () => {
+    cy.get("#project-1").within(() => {
+      cy.get(".image"), should("exist");
+      cy.get(".ui.header").should("contain", "My First Website");
+      cy.get(".description").should(
+        "contain",
+        "this was my first project. The guy lost but won the popular vote"
+      );
+    });
+  });
+
+  it("displays second project", () => {
+    cy.get("#project-2").within(() => {
+      cy.get(".image"), should("exist");
+      cy.get(".ui.header").should("contain", "UI Design");
+      cy.get(".description").should(
+        "contain",
+        "Designing user interface is fun."
+      );
+    });
+  });
+
+  it("displays third project", () => {
+    cy.get("#project-3").within(() => {
+      cy.get(".image"), should("exist");
+      cy.get(".ui.header").should("contain", "Mobile UX");
+      cy.get(".description").should(
+        "contain",
+        "this was my first project. The guy lost but won the popular vote"
+      );
+    });
+  });
+  
+});
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Projects = () => {
-  return (
-    <div>
-      <h1 id="about-header" className="ui header">About Me</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      Labore ab, expedita unde libero velit assumenda. Corrupti
-      qui natus dolore tenetur reprehenderit. Fuga iusto distinctio
-      veritatis! Unde ipsam necessitatibus praesentium fuga.</p>
-    </div>
-  )
+class Projects extends Component {
+  state = {
+    projects: [],
+  };
+
+  componentDidMount(){
+    axios.get
+  }
+
+  render() {
+    const projects = this.state.projects;
+      let projectsList;
+
+    if (projects.length > 0) {
+      projectsList = projects.map((project) => {
+        return (
+          <div id={"project-" + project.id} key={project.id}>
+            <h3 className="ui header">{project.id} {project.name}</h3>
+          </div>
+        )
+      })
+    }
+    return (
+      <div className="ui main container">
+        <h1 id="projects-header" className="ui header">
+          My Projects
+        </h1>
+        {projectsList}
+      </div>
+    )
+  }
 }
 
 export default Projects
